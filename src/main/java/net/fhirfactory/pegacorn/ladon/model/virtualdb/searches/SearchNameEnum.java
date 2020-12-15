fc9fce0bc85af1eb83af0f52fc7eccc986fa39d9
Mark Hunter <m.a.hunter@outlook.com>
2020-12-15 18:46:07 +1100

    IIE to ARIA Working

--- src/main/java/net/fhirfactory/pegacorn/ladon/model/virtualdb/searches/SearchNameEnum.java
+++ src/main/java/net/fhirfactory/pegacorn/ladon/model/virtualdb/searches/SearchNameEnum.java
@@ -2,6 +2,7 @@ package net.fhirfactory.pegacorn.ladon.model.virtualdb.searches;
 
 public enum SearchNameEnum {
     DOCUMENT_REFERENCE_DATE_AND_TYPE("DocumentReference: date=[Start,End], type=[xxx]"),
+    PATIENT_QRY_A19("Pateint: identifier, queryString"),
     PROCEDURE_PATIENT_AND_DATE("Procedure: subject.where(resolve() is Patient), performed=[Start,End]");
 
     private String searchName;
